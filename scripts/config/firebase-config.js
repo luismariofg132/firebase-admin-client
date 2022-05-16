004059b16ac50b1a1acd7eac0569ec234c77e994
Luis Mario Franco <lmfg06@gmail.com>
2022-05-16 07:07:11 -0500

    Upload client firebase admin

---
+++ scripts/config/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyD526yoDb9GClGPDmkn-Q7iGN7yMFFEpR8",
+    authDomain: "tienda-jm-autentication.firebaseapp.com",
+    projectId: "tienda-jm-autentication",
+    storageBucket: "tienda-jm-autentication.appspot.com",
+    messagingSenderId: "14131413616",
+    appId: "1:14131413616:web:e1f0595dde0f85af456ab5"
+}
---
+++ scripts/home.js
@@ -0,0 +1,36 @@
+const token = localStorage.getItem('token') || ""
+const url = "http://localhost:3800/api/users"
+const tbody = document.getElementById('tbody')
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const res = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const users = await res.json()
+
+    showUsers(users)
+})
+
+const showUsers = (users) => {
+    tbody.innerHTML = ""
+    users.forEach(user => {
+        tbody.innerHTML += `
+        <tr>
+            <th>${user.id}</th>
+            <th>${user.documento}</th>
+            <th>${user.tipo_documento}</th>
+            <th>${user.nombres}</th>
+            <th>${user.apellidos}</th>
+            <th>${user.correo}</th>
+            <th>${user.telefono_celular}</th>
+            <th>${user.genero}</th>
+            <th>${user.fecha_nacimiento}</th>
+            <th>${user.fecha_actualizacion}</th>
+            <th>${user.fecha_creacion}</th> 
+        </tr>
+        `
+    })
+}
